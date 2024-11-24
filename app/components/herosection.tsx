3f5c1456270db0633f08ab6d8044d552d9136086
Error-614 <mahlabazam614@gmail.com>
2024-11-24 14:50:29 +0500

    first commit

---
+++ app/components/herosection.tsx
@@ -0,0 +1,17 @@
+export default function Herosection(){
+    return (
+        <div className="bg-amber-100">
+            <h1 className="text-2xl font-bold text-center bg-gray-400 h-12">class 5 assignment</h1>
+            <div className="flex justify-around mt-9">
+                <div className=" h-80 bg-red-600 w-96 rounded"></div>
+                <div className=" h-80 bg-green-600 w-96 rounded"></div>
+                <div className=" h-80 bg-blue-600 w-96 rounded"></div>
+            </div>
+            <div className="flex justify-around mt-9">
+                <div className=" h-80 bg-cyan-600 w-2/6 rounded"></div>
+                <div className=" h-80 bg-orange-600 w-2/6 rounded"></div>
+
+            </div>
+        </div>
+    )
+}
